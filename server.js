009b8b80c407a88e5a42072c8e2db87470182d2d
Borja Aranda <farandac@redhat.com>
2018-10-15 12:28:58 +0200

    Change response

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var hostname = os.hostname();
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n", hostname);
+  response.end(hostname);
 });
 
 server.listen(8080);
